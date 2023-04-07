94dd6aaf9147ae1d6a810b975613f21da36e4c22
visvanathan.tharvinash.external@ssi-schaefer.com <visvanathan.tharvinash.external@ssi-schaefer.com>
2023-04-08 00:25:07 +0800

    'latest'

--- Main/views.py
+++ Main/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
+    # response = render(request, 'Main/index.html')
+    # return HttpResponse(response)
     try:
         response = render(request, 'Main/index.html')
         return HttpResponse(response)
