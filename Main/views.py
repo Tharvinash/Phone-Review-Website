4e56a6d528e1b725f3a9eaa99ff8602ed7ac96ec
visvanathan.tharvinash.external@ssi-schaefer.com <visvanathan.tharvinash.external@ssi-schaefer.com>
2023-04-09 01:31:28 +0800

    data integration

--- Main/views.py
+++ Main/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from PhoneReview.models import Review
 
 # Create your views here.
 def main(request):
     # response = render(request, 'Main/index.html')
     # return HttpResponse(response)
     try:
-        response = render(request, 'Main/index.html')
+        reviews = Review.objects.all()
+        response = render(request, 'Main/index.html', {'reviews': reviews})
         return HttpResponse(response)
     except:
         return HttpResponseNotFound('<h1>error main page</h1>')
