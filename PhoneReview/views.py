da07152414ef9be111c64c0dd2314d2cd2e5b752
visvanathan.tharvinash.external@ssi-schaefer.com <visvanathan.tharvinash.external@ssi-schaefer.com>
2023-04-11 22:43:04 +0800

    Fix login

--- PhoneReview/views.py
+++ PhoneReview/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
-from PhoneReview.models import Brand, PhoneModel
+from PhoneReview.models import Review, Brand, PhoneModel
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -13,24 +13,20 @@ def main(request):
         return HttpResponseNotFound('<h1>Phone models page crashed</h1>')
     
 def specificModel(request, model):    
+    try:
         brand = Brand.objects.get(name=model)
         logging.debug(f"model: {model}")
         logging.debug(f"brand: {brand}")
         brand_phones = PhoneModel.objects.filter(brand=brand)
         response = render(request, 'PhoneReview/specificModels.html', {'models': brand_phones, 'brand': model})
         return HttpResponse(response)
-    # try:
-    # except:
-    #     return HttpResponseNotFound('<h1>Specific models page crashed</h1>')
+    except:
+        return HttpResponseNotFound('<h1>Specific models page crashed</h1>')
     
 def reviews(request, model, slug):    
     try:
-        brand = Brand.objects.get(name='Vivo')
-        logging.debug(f"model: {slug}")
-        logging.debug(f"brand: {brand}")
-        brand_phones = PhoneModel.objects.filter(brand=brand)
-        logging.debug(f"brand_phones: {brand_phones}")
-        response = render(request, 'PhoneReview/specificModels.html', {'models': brand_phones})
+        reviews = Review.objects.filter(models__slug=slug)
+        response = render(request, 'Main/index.html', {'reviews': reviews})
         return HttpResponse(response)
     except:
         return HttpResponseNotFound('<h1>Review page crashed</h1>')
