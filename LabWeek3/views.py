a4f05485dfa23cb158c7ab7c46a6f926abc6dbc9
visvanathan.tharvinash.external@ssi-schaefer.com <visvanathan.tharvinash.external@ssi-schaefer.com>
2023-04-26 01:10:32 +0800

    save data to db

--- LabWeek3/views.py
+++ LabWeek3/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from PhoneReview.models import PhoneModel
 from .forms import ReviewForm
+from django.urls import reverse
+from django.utils import timezone
 
 # Create your views here.
 def login(request):
@@ -19,19 +21,21 @@ def register(request):
         return HttpResponseNotFound('<h1>Register page crashed</h1>')
 
 def addReview(request):
-    phoneModels = PhoneModel.objects.all()
     if request.method == 'POST':
         form = ReviewForm(request.POST)
-
         if form.is_valid():
+            current_datetime = timezone.now()
+            form.instance.date_published = current_datetime
+
             form.save()
+            redirect_url = reverse('model-list')
+            return HttpResponseRedirect(redirect_url)
     else:
         form = ReviewForm()
 
     try:
         response = render(request, 'LabWeek3/add-review.html', {
             'form': form,
-            'phoneModels': phoneModels
             })
         return HttpResponse(response)
     except:
